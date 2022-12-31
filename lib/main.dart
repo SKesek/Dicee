c5078b856393ce90351dbc831804efe85b48cbba
Krzysztof Swend <krzysiekswend@gmail.com>
2022-12-31 16:37:29 +0100

    randomized dice
    full project completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -10,30 +11,72 @@ void main() {
           title: const Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 6;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: TextButton(
-              child: Image.asset('images/dice1.png'),
-              onPressed: () {},
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                    });
+                  },
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: TextButton(
-              child: Image.asset('images/dice5.png'),
-              onPressed: () {},
-            ),
-          ),
+          OutlinedButton(
+              onPressed: () {
+                rollDice();
+              },
+              style: OutlinedButton.styleFrom(
+                minimumSize: const Size(200, 50),
+                side: const BorderSide(width: 5, color: Colors.white),
+              ),
+              child: const Text(
+                'Roll',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ))
         ],
       ),
     );
